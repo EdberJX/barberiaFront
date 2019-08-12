1e836600fcdbe97112507cd631f244d93d8b898b
EdberJX react <edbertaboada@gmail.com>
2019-08-12 15:16:56 -0500

    graficas listas

--- src/components/estadistica.js
+++ src/components/estadistica.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import {Bar} from 'react-chartjs-2';
+import './card.css';
+//import GraficoBarras from './GraficoBarras';
+
 export default class Estadistica extends Component{
     
     async componentDidMount(){
@@ -8,18 +12,58 @@ export default class Estadistica extends Component{
 
     }
     async getEstadistica(){
-        const {data} = await axios.get("http://localhost:5000/estadisticas/"+this.state.idbarberia)
-        console.log("s",data)
+        const res = await axios.get("http://localhost:5000/estadisticas/"+this.state.idbarberia)
+        const nombre = res.data.map( barbe =>barbe.nombre);
+        const servicios = res.data.map( barbe =>barbe.citasAtendidas);
+        console.log(servicios)
+        const data = {
+            labels: nombre,
+            datasets: [
+              {
+                label: 'Barberos vs Servicios realizados',
+                backgroundColor: '#fffd82',
+                borderColor: 'black',
+                borderWidth: 1,
+                hoverBackgroundColor: 'red',
+                hoverBorderColor: 'green',
+                data: servicios
+              }
+            ]
+        };
+        this.setState({
+            barberosEstadistica: res.data,
+        data: data
+        })
+        console.log(this.state.barberosEstadistica)
+        
     }
+    
     state = {
         idbarberia: "",
-        barberosEstadistica:[]
+        barberosEstadistica:[],
+        data:[],
+        displayName: 'BarExample'
     }
     render(){
         return(
-            <div>
-                hola
+            
+            <div className=" text-center id="esta>
+                 <div>
+              <h2>Servicios atendios por barberos</h2>
+              <Bar
+                data={this.state.data}
+                width={100}
+                height={50}
+                options={{
+                  maintainAspectRatio: false
+                }}
+              />
             </div>
+            </div> 
+          
         )
     }
 }
+/**
+ * //<GraficoBarras barberosEstadistica={this.barberosEstadistica}/>
+ */
